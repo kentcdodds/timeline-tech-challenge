3f44679504b6a2613037e7b270a56436a8e165c8
Matt Zabriskie <mzabriskie@gmail.com>
2014-07-02 21:57:29 -0600

    Using whitelist to protect files from prying eyes

--- index.js
+++ index.js
@@ -5,8 +5,39 @@ var argv = require('minimist')(process.argv.slice(2)),
     path = require('path'),
 	server;
 
+// Respond to request with a 404 error
+function send404(res) {
+	res.writeHead(404);
+	res.write('404 Not Found');
+	res.end();
+}
+
 server = http.createServer(function (req, res) {
-    fs.createReadStream(path.join(__dirname, 'index.html')).pipe(res);
+	console.log(req.url);
+
+	var url = null,
+		file;
+
+	// Whitelist files so that protected files can't be requested
+	if (req.url === '/' || req.url === '/index.html') {
+		url = 'index.html'
+	} else if (req.url === '/timeline.json') {
+		url = 'timeline.json';
+	}
+
+	// If file exists pipe it to response, otherwise send 404
+	if (url !== null) {
+		file = path.join(__dirname, url);
+		fs.exists(file, function (exists) {
+			if (!exists) {
+				send404(res);
+			} else {
+				fs.createReadStream(file).pipe(res);
+			}
+		});
+	} else {
+		send404(res);
+	}
 });
 
 server.listen(port);
